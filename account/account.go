db0936d1ffd0f49c663817f0fcf6133a42b7215d
nicolas-martin <nicolas.martin.0986@gmail.com>
2021-06-01 12:33:29 -0400

    more convert testing

--- account/account.go
+++ account/account.go
@@ -2,13 +2,14 @@ package account
 
 import (
 	"crypto/sha256"
+	"encoding/base32"
 	"encoding/binary"
 	"encoding/hex"
 	"hash/crc32"
+	"strings"
 
 	"github.com/btcsuite/btcd/btcec"
 	"github.com/dfinity/keysmith/codec"
-	"github.com/dfinity/keysmith/principal"
 )
 
 type AccountId interface {
@@ -26,7 +27,7 @@ func FromECPubKey(pubKey *btcec.PublicKey) (AccountId, error) {
 	}
 	hash := sha256.New224()
 	hash.Write([]byte("\x0Aaccount-id"))
-	hash.Write(principal.NewSelfAuthenticatingId(der).Bytes())
+	hash.Write(NewSelfAuthenticatingId(der).Bytes())
 	hash.Write(make([]byte, 32))
 	data := hash.Sum(nil)
 	return &accountId{data: data}, nil
@@ -36,4 +37,56 @@ func (accountId *accountId) String() string {
 	crc := make([]byte, 4)
 	binary.BigEndian.PutUint32(crc, crc32.ChecksumIEEE(accountId.data))
 	return hex.EncodeToString(append(crc, accountId.data...))
+}
+
+func NewSelfAuthenticatingId(der []byte) PrincipalId {
+	hash := sha256.Sum224(der)
+	data := append(hash[:], []byte{2}...)
+	return &SelfAuthenticatingId{data: data}
+}
+
+type PrincipalId interface {
+	Bytes() []byte
+	String() string
+}
+
+type SelfAuthenticatingId struct {
+	data []byte
+}
+
+func (principalId *SelfAuthenticatingId) Bytes() []byte {
+	return principalId.data
+}
+
+func (principalId *SelfAuthenticatingId) String() string {
+	return show(principalId.data)
+}
+
+func show(data []byte) string {
+	crc := make([]byte, 4)
+	binary.BigEndian.PutUint32(crc, crc32.ChecksumIEEE(data))
+	encoder := base32.StdEncoding.WithPadding(base32.NoPadding)
+	str := encoder.EncodeToString(append(crc, data...))
+	return strings.Join(split(strings.ToLower(str), 5), "-")
+}
+
+func split(str string, n int) []string {
+	if n >= len(str) {
+		return []string{str}
+	}
+	var chunks []string
+	chunk := make([]rune, n)
+	i := 0
+	for _, r := range str {
+		chunk[i] = r
+		i++
+		if i == n {
+			chunks = append(chunks, string(chunk))
+			i = 0
+		}
+	}
+	if i > 0 {
+		chunks = append(chunks, string(chunk[:i]))
+	}
+	return chunks
 }
--- account/account_test.go
+++ account/account_test.go
@@ -1,21 +1,90 @@
 package account
 
 import (
+	"crypto/elliptic"
+	"crypto/sha256"
+	"encoding/asn1"
 	"encoding/hex"
+	"encoding/json"
+	"fmt"
 	"testing"
 
 	"github.com/btcsuite/btcd/btcec"
+	"github.com/nicolas-martin/key-convert/codec"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestFromECPubKey(t *testing.T) {
-	accountID := accountId{data: []byte("02f2326544f2040d3985e31db5e7021402c541d3cde911cd20e951852ee4da47")}
+	icpAccountID := &accountId{data: []byte("02f2326544f2040d3985e31db5e7021402c541d3cde911cd20e951852ee4da47")}
 
-	rawPk, _ := hex.DecodeString("04931f4d17682b3dedbbd87d29d86040ee5f361a182045c3475ff2fc05af7e30a79f1040784655b226520dbb71aac3edc951cd50a64c138596ef746265e4cf122f")
+	rawPk, err := hex.DecodeString("04931f4d17682b3dedbbd87d29d86040ee5f361a182045c3475ff2fc05af7e30a79f1040784655b226520dbb71aac3edc951cd50a64c138596ef746265e4cf122f")
+	assert.NoError(t, err)
+
 	pk, err := btcec.ParsePubKey(rawPk, btcec.S256())
-	assert.Nil(t, err)
+	assert.NoError(t, err)
 
 	got, err := FromECPubKey(pk)
 	assert.Nil(t, err)
-	assert.Equal(t, got, accountID)
+	fmt.Printf("%s\n", got.String())
+	fmt.Printf("%s\n", icpAccountID.data)
+	assert.Equal(t, string(icpAccountID.data), got.String())
+}
+
+func TestEncodePubKey(t *testing.T) {
+	rawPk, err := hex.DecodeString("04931f4d17682b3dedbbd87d29d86040ee5f361a182045c3475ff2fc05af7e30a79f1040784655b226520dbb71aac3edc951cd50a64c138596ef746265e4cf122f")
+	assert.NoError(t, err)
+
+	pubKey, err := btcec.ParsePubKey(rawPk, btcec.S256())
+	assert.NoError(t, err)
+
+	curve := btcec.S256()
+	point := pubKey.ToECDSA()
+
+	elliptic := elliptic.Marshal(curve, point.X, point.Y)
+	fmt.Printf("%x\n", elliptic)
+	// Output: 04931f4d17682b3dedbbd87d29d86040ee5f361a182045c3475ff2fc05af7e30a79f1040784655b226520dbb71aac3edc951cd50a64c138596ef746265e4cf122f
+
+	ellipTicbitString := asn1.BitString{Bytes: elliptic}
+
+	ecppubkey := codec.ECPubKey{
+		Metadata: []asn1.ObjectIdentifier{
+			asn1.ObjectIdentifier{1, 2, 840, 10045, 2, 1},
+			asn1.ObjectIdentifier{1, 3, 132, 0, 10},
+		},
+		PublicKey: ellipTicbitString,
+	}
+	bEcppubkey, _ := json.Marshal(ecppubkey)
+	fmt.Println(string(bEcppubkey))
+	// Output: {"Metadata":[[1,2,840,10045,2,1],[1,3,132,0,10]],"PublicKey":{"Bytes":"BJMfTRdoKz3tu9h9KdhgQO5fNhoYIEXDR1/y/AWvfjCnnxBAeEZVsiZSDbtxqsPtyVHNUKZME4WW73RiZeTPEi8=","BitLength":0}}
+
+	marshalledKey, err := asn1.Marshal(ecppubkey)
+	assert.NoError(t, err)
+	jsonMarshalledKey, _ := json.Marshal(marshalledKey)
+	fmt.Println(string(jsonMarshalledKey))
+	// Output: "MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEkx9NF2grPe272H0p2GBA7l82GhggRcNHX/L8Ba9+MKefEEB4RlWyJlINu3Gqw+3JUc1QpkwThZbvdGJl5M8SLw=="
+}
+
+func TestRestEncode(t *testing.T) {
+	rawPk, err := hex.DecodeString("04931f4d17682b3dedbbd87d29d86040ee5f361a182045c3475ff2fc05af7e30a79f1040784655b226520dbb71aac3edc951cd50a64c138596ef746265e4cf122f")
+	assert.NoError(t, err)
+
+	pubKey, err := btcec.ParsePubKey(rawPk, btcec.S256())
+	assert.NoError(t, err)
+
+	der, err := codec.EncodeECPubKey(pubKey)
+	assert.NoError(t, err)
+	selfId := NewSelfAuthenticatingId(der)
+	fmt.Printf("%s\n", selfId)
+	// Output: tjpnz-kfh3h-es2ok-k7wp4-ieiad-qvntd-hd4k3-zxdlf-tg3of-l37zo-7ae
+
+	hash := sha256.New224()
+	hash.Write([]byte("\x0Aaccount-id"))
+	hash.Write(selfId.Bytes())
+	hash.Write(make([]byte, 32))
+	data := hash.Sum(nil)
+	d := &accountId{data: data}
+
+	fmt.Printf("%s\n", d.String())
+	// Output: 02f2326544f2040d3985e31db5e7021402c541d3cde911cd20e951852ee4da47
+
 }
